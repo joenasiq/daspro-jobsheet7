f8f538dc378547d4ebddf52dc2dd9f56b740069a
joenasiq <arjunash2006@gmail.com>
2024-10-09 14:48:00 +0700

    commit tugas

---
+++ KafeDoWhile06.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class KafeDoWhile06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int kopi, teh, roti, hargaKopi=12000, hargaTeh=7000, hargaRoti=20000;
+        String namaPelanggan;
+        double totalHarga;
+        do {
+            System.out.print("masukkan nama pelanggan (ketik 'batal' untuk keluar): ");
+            namaPelanggan = sc.nextLine();
+            if (namaPelanggan.equalsIgnoreCase("batal")) {
+                System.out.println("semua transaksi telah selesai");
+                break;
+            }
+            System.out.print("jumlah kopi: ");
+            kopi = sc.nextInt();
+            System.out.print("jumlah teh: ");
+            teh = sc.nextInt();
+            System.out.print("jumlah roti: ");
+            roti = sc.nextInt();
+            totalHarga = (kopi * hargaKopi) + (teh * hargaTeh) + (roti * hargaRoti);
+            System.out.println("total yang harus dibayar: " + totalHarga);
+            sc.nextLine();
+        } while (true);
+    }
+}
